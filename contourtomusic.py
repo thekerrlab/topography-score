0f96368cdaa5019fba6b6c939872d4e57dee8257
cliffckerr <questionably@gmail.com>
2017-10-10 04:33:13 +0100

    no luck with midi but rest sounds amazing

--- contourtomusic.py
+++ contourtomusic.py
@@ -3,17 +3,23 @@ Version:
 """
 
 from pylab import loadtxt, plot, interp, arange, linspace
-from playpiece import note
+from playpiece import makenote, play
+from midiutil.MidiFile import MIDIFile
 
 
 # Parameters
 trimstart = 200
 trimend = 400
 fn = 'rawcontour.txt'
+midifn = 'contours.mid'
 octaves = 4
 seconds = 180
 notesperoctave = 12
 notespersecond = 4
+converttonotes = False
+converttomidi = True
+
+duration = 1#1.0/notespersecond
 
 def makepitch(val, fundamental=100):
     ''' Turn a number of semitones into a pitch '''
@@ -35,15 +41,35 @@ maxnotes = octaves*notesperoctave
 contour -= contour.min()
 contour *= maxnotes/float(contour.max())
 contour = contour.round()
-plot(contour)
+#plot(contour)
 
-# Convert to notes
-pitches = []
-for val in contour:
-    pitches.append(makepitch(val))
-
-# Play
-duration = 10.0/notespersecond
-for pitch in pitches:
-    print pitch
-#    note(frequency=pitch, duration=duration)
+#%%
+if converttonotes:
+    # Convert to notes
+    pitches = []
+    for val in contour:
+        pitches.append(makepitch(val))
+    # Play
+    
+    notes = []
+    for pitch in pitches:
+        print pitch
+        thisnote = makenote(frequency=pitch, duration=duration)
+        notes += thisnote
+    play(notes)
+    
+    
+#%%
+if converttomidi:
+    mf = MIDIFile(1)     # create your MIDI object
+    track = 0   # the only track
+    time = 0    # start at the beginning
+    channel = 0
+    volume = 100
+    time = 0             # start on beat 0
+    mf.addTrackName(track, time, "Sample Track")
+    mf.addTempo(track, time, 60)
+    for pitch in contour: # add some notes
+        mf.addNote(track, channel, pitch, time, duration, volume)
+    with open(midifn, 'wb') as outf: mf.writeFile(outf) # write it to disk
+        
