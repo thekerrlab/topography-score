da16f4fa5d96f7520f136809662691bcc1a5a9e5
cliffckerr <questionably@gmail.com>
2017-10-10 02:39:29 +0100

    yay this works

--- elevation.py
+++ elevation.py
@@ -1,20 +1,21 @@
+#%%
 print('Importing libraries...')
 from osgeo import gdal
-import matplotlib.pyplot as plt
-from pylab import *
+from pylab import array, zeros, figure, imshow, nan
 
+#%%
 print('Defining parameters...')
 folder = '/u/cliffk/cliff/music/london2017/elevationdata/'
 controlstr = 'n%02i_e%03i_1arc_v3.tif'
 North = [41, 42, 43]
 East = range(39,49)
 
+#%%
+print('Loading files...')
 data = []
 ncoords = []
 ecoords = []
 nfiles = 0
-
-print('Loading files...')
 for north in North:
     for east in East:
         string = controlstr % (north, east)
@@ -30,6 +31,7 @@ for north in North:
         except Exception as E:
             pass # print('No matter: %s' % E.__repr__())
 
+#%%
 print('Processing...')
 npts = elevation.shape[0]
 ncoords = array(ncoords)
@@ -38,21 +40,25 @@ ncoords -= ncoords.min()
 ecoords -= ecoords.min()
 northpts = npts*(ncoords.max()+1)
 eastpts  = npts*(ecoords.max()+1)
-
-fulldata = zeros((northpts,eastpts))
+fulldata = zeros((northpts,eastpts))+nan
 for f in range(nfiles):
-    npos = ncoords[f]*npts
+    npos = (ncoords.max()-ncoords[f])*npts
     epos = ecoords[f]*npts
     thisdata = data[f]
     fulldata[npos:npos+npts, epos:epos+npts] = thisdata
 
+#%%
+print('Subsampling...')
+subsamp = int(eastpts/1980)
+subdata = fulldata[::subsamp,::subsamp]
+
+#%%
 print('Plotting...')
 figure(facecolor='w')
-imshow(fulldata, cmap='gist_earth')
+imshow(subdata, cmap='gist_earth')
 
 #vector = []
 #for i in range(elevation.shape[0]):
 #    vector.append(elevation[i,i])
-#
 #plt.figure(facecolor='w')
 #plt.plot(vector)
