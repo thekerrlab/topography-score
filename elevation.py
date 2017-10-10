8890c69c72736e3f4f947b8273c95f5568e5b45d
cliffckerr <questionably@gmail.com>
2017-10-10 02:27:13 +0100

    works, but crashed

--- elevation.py
+++ elevation.py
@@ -1,24 +1,58 @@
+print('Importing libraries...')
 from osgeo import gdal
-gdal.UseExceptions()
-
-fn = '/u/cliffk/cliff/music/london2017/elevation/n41_e048_1arc_v3.tif'
-fn2 = '/u/cliffk/cliff/music/london2017/elevation/n41_e047_1arc_v3.tif'
-
-ds = gdal.Open(fn2)
-band = ds.GetRasterBand(1)
-elevation = band.ReadAsArray()
-
-print elevation.shape
-print elevation
-
-plt.figure(facecolor='w')
 import matplotlib.pyplot as plt
-plt.imshow(elevation, cmap='gist_earth')
-plt.show()
+from pylab import *
 
-vector = []
-for i in range(elevation.shape[0]):
-    vector.append(elevation[i,i])
+print('Defining parameters...')
+folder = '/u/cliffk/cliff/music/london2017/elevationdata/'
+controlstr = 'n%02i_e%03i_1arc_v3.tif'
+North = [41, 42, 43]
+East = range(39,49)
 
-plt.figure(facecolor='w')
-plt.plot(vector)
+data = []
+ncoords = []
+ecoords = []
+nfiles = 0
+
+print('Loading files...')
+for north in North:
+    for east in East:
+        string = controlstr % (north, east)
+        fn = folder+string
+        print('Loading %s...' % fn)
+        try:
+            ds = gdal.Open(fn)
+            elevation = ds.GetRasterBand(1).ReadAsArray()
+            data.append(elevation)
+            ncoords.append(north)
+            ecoords.append(east)
+            nfiles += 1
+        except Exception as E:
+            pass # print('No matter: %s' % E.__repr__())
+
+print('Processing...')
+npts = elevation.shape[0]
+ncoords = array(ncoords)
+ecoords = array(ecoords)
+ncoords -= ncoords.min()
+ecoords -= ecoords.min()
+northpts = npts*(ncoords.max()+1)
+eastpts  = npts*(ecoords.max()+1)
+
+fulldata = zeros((northpts,eastpts))
+for f in range(nfiles):
+    npos = ncoords[f]*npts
+    epos = ecoords[f]*npts
+    thisdata = data[f]
+    fulldata[npos:npos+npts, epos:epos+npts] = thisdata
+
+print('Plotting...')
+figure(facecolor='w')
+imshow(fulldata, cmap='gist_earth')
+
+#vector = []
+#for i in range(elevation.shape[0]):
+#    vector.append(elevation[i,i])
+#
+#plt.figure(facecolor='w')
+#plt.plot(vector)
