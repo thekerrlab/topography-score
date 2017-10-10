b688fb53950d53d518abf12672fa63b682e133f1
cliffckerr <questionably@gmail.com>
2017-10-10 04:45:50 +0100

    mwhahaha

--- contourtomusic.py
+++ contourtomusic.py
@@ -61,15 +61,20 @@ if converttonotes:
     
 #%%
 if converttomidi:
-    mf = MIDIFile(1)     # create your MIDI object
-    track = 0   # the only track
-    time = 0    # start at the beginning
-    channel = 0
-    volume = 100
-    time = 0             # start on beat 0
-    mf.addTrackName(track, time, "Sample Track")
-    mf.addTempo(track, time, 60)
-    for pitch in contour: # add some notes
-        mf.addNote(track, channel, pitch, time, duration, volume)
-    with open(midifn, 'wb') as outf: mf.writeFile(outf) # write it to disk
+    offset = 30
+    degrees  = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
+    track    = 0
+    channel  = 0
+    time     = 0    # In beats
+    duration = 1    # In beats
+    tempo    = 60   # In BPM
+    volume   = 100  # 0-127, as per the MIDI standard
+    MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+    MyMIDI.addTempo(track, time, tempo)
+    for i, pitch in enumerate(contour):
+        print pitch
+        MyMIDI.addNote(track, channel, pitch+offset, time + i, duration, volume)
+    
+    with open(midifn, "wb") as output_file:
+        MyMIDI.writeFile(output_file)
         
